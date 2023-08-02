691c9a238d9bf9cc59a754e207769c75a9254b6f
junha <goodjunha@gmail.com>
2023-08-02 15:23:10 +0900

    💵 :: BaseVC 생성

--- Features/Start/StartUserInterface/DemoApp/Sources/StartUserInterfaceViewController.swift
+++ Features/Start/StartUserInterface/DemoApp/Sources/StartUserInterfaceViewController.swift
@@ -1,16 +1,43 @@
 import UIKit
-import ResourceKit
+import SuperUI
 import Then
+import ResourceKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
-class StartUserInterfaceViewController: UIViewController {
-    
-    var c = CustomButton(title: "asdf", backgroundColor: .black, titleColor: .black).then {
-        $0.backgroundColor = .red
+final class StartUserInterfaceViewController: BaseViewController {
+    private let disposeBag = DisposeBag()
+    private let imageOptions: [UIImage] = [ResourceKitAsset.sBackground1.image]
+    private let currentImageRelay = BehaviorRelay<UIImage?>(value: nil)
+
+    private let imageView = UIImageView().then {
+        $0.image = ResourceKitAsset.sBackground1.image
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
     }
 
+    override func layout() {
+        view.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    override func bindActions() {
+        currentImageRelay
+            .bind(to: imageView.rx.image)
+            .disposed(by: disposeBag)
+    }
+
+    private func displayRandomImage() {
+        let randomImage = imageOptions.randomElement()
+        currentImageRelay.accept(randomImage)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemPink
+        
+        displayRandomImage()
     }
 }
-
