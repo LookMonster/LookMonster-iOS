13b4fb39cf93cf15dafcfd2bd29671fd602a6f36
junha <goodjunha@gmail.com>
2023-08-10 16:37:24 +0900

    🥊:: 스타일 탭바 변경

--- Features/User/Main/Style/StyleUserInterface/Implement/AppStyleImpl/StyleHomeViewController.swift
+++ Features/User/Main/Style/StyleUserInterface/Implement/AppStyleImpl/StyleHomeViewController.swift
@@ -11,41 +11,35 @@ import UIKit
 import StyleUserInterface
 
 protocol StyleHomePresentableListener: AnyObject {
-  // TODO: Declare properties and methods that the view controller can invoke to perform
-  // business logic, such as signIn(). This protocol is implemented by the corresponding
-  // interactor class.
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
 }
 
 final class StyleHomeViewController: UIViewController, StyleHomePresentable, StyleHomeViewControllable {
-  
-  weak var listener: StyleHomePresentableListener?
-  
-  init() {
-    super.init(nibName: nil, bundle: nil)
     
-    setupViews()
-  }
-  
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
+    weak var listener: StyleHomePresentableListener?
     
-    setupViews()
-  }
-  
-  private let label: UILabel = {
-    let label = UILabel()
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
-  
-  func setupViews() {
-    tabBarItem = UITabBarItem(title: "프로필", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
-    label.text = "Profile Home"
-    view.backgroundColor = .systemTeal
-    view.addSubview(label)
-    NSLayoutConstraint.activate([
-      label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-    ])
-  }
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupViews()
+    }
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        tabBarItem = UITabBarItem(title: "프로필", image: UIImage(systemName: "creditcard"), selectedImage: UIImage(systemName: "creditcard.fill"))
+        view.backgroundColor = .red
+    }
 }
