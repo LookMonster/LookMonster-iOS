dc599ea256f1aaedc03671bd072d33aa4ceba2bc
junha <goodjunha@gmail.com>
2023-08-10 21:03:32 +0900

    🥊:: 홈화면 안쓰는거 정리

--- Features/User/Main/Home/HomeUserInterface/Implement/AppHomeImpl/AppHomeViewController.swift
+++ Features/User/Main/Home/HomeUserInterface/Implement/AppHomeImpl/AppHomeViewController.swift
@@ -9,16 +9,6 @@ final class AppHomeViewController: UIViewController, AppHomePresentable, AppHome
   
   weak var listener: AppHomePresentableListener?
   
-  private let widgetStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    stackView.axis = .horizontal
-    stackView.distribution = .fillEqually
-    stackView.alignment = .top
-    stackView.spacing = 20
-    return stackView
-  }()
-  
   init() {
     super.init(nibName: nil, bundle: nil)
     
@@ -32,25 +22,12 @@ final class AppHomeViewController: UIViewController, AppHomePresentable, AppHome
   }
   
   func updateWidget(_ viewModels: [HomeWidgetViewModel]) {
-    let views = viewModels.map { HomeWidgetView(viewModel: $0) }
-    
-    views.forEach {
-      $0.addShadowWithRoundedCorners(12)
-      widgetStackView.addArrangedSubview($0)
-    }
   }
   
   private func setupViews() {
     title = "홈"
-      tabBarItem = UITabBarItem(title: "홈", image: ResourceKitAsset.homeImage.image, tag: 0)
+      tabBarItem = UITabBarItem(title: "Home", image: ResourceKitAsset.homeImage.image, tag: 0)
     view.backgroundColor = .backgroundColor
-    view.addSubview(widgetStackView)
-    
-    NSLayoutConstraint.activate([
-      widgetStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-      widgetStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-      widgetStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-    ])
   }
   
 }
