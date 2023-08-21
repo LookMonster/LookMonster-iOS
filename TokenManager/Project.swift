ee376c165e17dcb9fdf4064728c7ddb13f0fef58
junha <goodjunha@gmail.com>
2023-08-21 14:21:36 +0900

    ♟️:: 토큰 모듈 추가

---
+++ TokenManager/Project.swift
@@ -0,0 +1,21 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 박준하 on 2023/08/21.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import UtilityPlugin
+
+let project = Project.invertedDualTargetProject(
+    name: "TokenManager",
+    platform: .iOS,
+    iOSTargetVersion: "16.0.0",
+    interfaceDependencies: [
+    ],
+    implementDependencies: [
+        .Core.CSLogger
+    ]
+)
