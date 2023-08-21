412457dcdb11f2db8ca2ded89a4146d4af340553
junha <goodjunha@gmail.com>
2023-08-21 17:01:46 +0900

    ♟️:: 디자인 시스템 구조 구성

--- DesignSystem/Project.swift
+++ DesignSystem/Project.swift
@@ -5,4 +5,69 @@
 //  Created by 박준하 on 2023/08/21.
 //
 
-import Foundation
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import UtilityPlugin
+
+let project = Project(
+    name: "DesignSystem",
+    organizationName: "com.monster.lookMonster",
+//    settings: .settings(configurations: [
+//        .debug(name: "Debug", xcconfig: .relativeToRoot("Config/Debug.xcconfig")),
+//        .release(name: "Release", xcconfig: .relativeToRoot("Config/Release.xcconfig")),
+//    ]),
+    targets: [
+        Target(
+            name: "DesignSystem",
+            platform: .iOS,
+            product: .framework,
+            bundleId: "com.monster.lookMonster",
+            deploymentTarget: .iOS(
+                targetVersion: "16.0.0",
+                devices: [.iphone]
+            ),
+            infoPlist: .default,
+            sources: ["Sources/**"],
+            dependencies: [
+                .ThirdParty.SPM.SnapKit,
+                .ThirdParty.SPM.Then,
+                .ResourceKit.Implement,
+                .ThirdParty.SPM.RxCocoa,
+                .ThirdParty.SPM.RxSwift,
+                .ThirdParty.SPM.Kingfisher,
+            ]),
+        Target(
+            name: "DesignSystemDemoApp",
+            platform: .iOS,
+            product: .app,
+            bundleId: "com.monster.lookMonster",
+            deploymentTarget: .iOS(
+                targetVersion: "16.0.0",
+                devices: [.iphone]
+            ),
+            infoPlist: InfoPlist.extendingDefault(
+                with:
+                    [
+                        "CFBundleDevelopmentRegion": "ko_KR",
+                        "CFBundleShortVersionString": "1.0",
+                        "CFBundleVersion": "1",
+                        "UILaunchStoryboardName": "LaunchScreen"
+                    ]
+            ),
+            sources: ["./DemoApp/Sources/**"],
+            resources: ["./DemoApp/Resources/**"],
+            dependencies:
+                [
+                    .target(name: "DesignSystem"),
+                    .ThirdParty.SPM.SnapKit,
+                    .ThirdParty.SPM.Then,
+                    .ResourceKit.Implement,
+                    .ThirdParty.SPM.RxCocoa,
+                    .ThirdParty.SPM.RxSwift,
+                    .ThirdParty.SPM.Kingfisher,
+                ]
+        )
+        
+    ]
+)
