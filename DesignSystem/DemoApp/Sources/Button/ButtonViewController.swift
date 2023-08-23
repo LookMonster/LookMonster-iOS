14905f7c0c5801997570fac026e9f79f4b9b4497
junha <goodjunha@gmail.com>
2023-08-23 15:16:11 +0900

    🕯️:: 버튼 예제 코드 추가

--- DesignSystem/DemoApp/Sources/Button/ButtonViewController.swift
+++ DesignSystem/DemoApp/Sources/Button/ButtonViewController.swift
@@ -16,7 +16,11 @@ import ResourceKit
 public class ButtonViewController: UIViewController {
     private let disposeBag = DisposeBag()
     
+    // Auth 🍎
     var AppleButton = MonsterAuthButton(image: ResourceKitAsset.appleIcon.image, title: "apple로 시작하기", backgorundColor: .black, titleColor: .white)
+    
+    // basic 🎱
+    var nextButton = MonsterButton(title: "다음", backgorundColor: .black, titleColor: .white)
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +28,16 @@ public class ButtonViewController: UIViewController {
         view.backgroundColor = .white
         
         view.addSubview(AppleButton)
+        view.addSubview(nextButton)
         
         AppleButton.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview().inset(24.0)
+            $0.height.equalTo(48.0)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.top.equalTo(AppleButton.snp.bottom).offset(30.0)
             $0.leading.trailing.equalToSuperview().inset(24.0)
             $0.height.equalTo(48.0)
         }
