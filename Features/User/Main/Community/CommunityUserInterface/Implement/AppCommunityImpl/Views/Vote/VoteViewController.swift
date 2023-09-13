91c5f6adf59ff75ed7abd1d5627143f2f8962e4c
junha <goodjunha@gmail.com>
2023-09-13 16:42:41 +0900

    🛒:: VateViewController collectionView

--- Features/User/Main/Community/CommunityUserInterface/Implement/AppCommunityImpl/Views/Vote/VoteViewController.swift
+++ Features/User/Main/Community/CommunityUserInterface/Implement/AppCommunityImpl/Views/Vote/VoteViewController.swift
@@ -1,10 +1,8 @@
 import UIKit
+import DesignSystem
 import CommunityUserInterface
 
-class VoteViewController: UIViewController, VotePresentable, VoteViewControllable, VoteListener  {
-    func communityBackground() {
-        print("asdf")
-    }
+class VoteViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, VotePresentable, VoteViewControllable, VoteListener  {
     
     var listener: VoteListener?
     
@@ -15,6 +13,48 @@ class VoteViewController: UIViewController, VotePresentable, VoteViewControllabl
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        self.collectionView!.register(CommunityCollectionViewCell.self, forCellWithReuseIdentifier: "CommunityCollectionViewCell")
     }
+    
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func communityBackground() {
+        print("asdf")
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CommunityCollectionViewCell", for: indexPath) as? CommunityCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.variousLabel = MonsterVariousLabel(text: "King_of_the_junha", type: .vote, timerType: .hoursAgo(4))
+        
+        return cell
+    }
+        
+    override func collectionView(_ collectionView :UICollectionView , didSelectItemAt indexPath :IndexPath){
+        print(indexPath.row)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let size = CGSize(width: UIScreen.main.bounds.size.width, height: 80)
+        return size
+    }
+    
 }
