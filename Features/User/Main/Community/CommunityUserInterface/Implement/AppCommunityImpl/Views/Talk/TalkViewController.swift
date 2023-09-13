a9f50798df6621d2857a6e7c20f444f118ce3cfa
junha <goodjunha@gmail.com>
2023-09-13 16:43:10 +0900

    🛒:: TaIkCollectionViewController

--- Features/User/Main/Community/CommunityUserInterface/Implement/AppCommunityImpl/Views/Talk/TalkViewController.swift
+++ Features/User/Main/Community/CommunityUserInterface/Implement/AppCommunityImpl/Views/Talk/TalkViewController.swift
@@ -1,10 +1,7 @@
 import UIKit
 import CommunityUserInterface
 
-class TalkViewController: UIViewController, TalkPresentable, TalkViewControllable, TalkListener  {
-    func communityBackground() {
-        print("asdf")
-    }
+class TalkViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, TalkPresentable, TalkViewControllable, TalkListener  {
     
     var listener: TalkListener?
     
@@ -15,6 +12,44 @@ class TalkViewController: UIViewController, TalkPresentable, TalkViewControllabl
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        self.collectionView!.register(CommunityCollectionViewCell.self, forCellWithReuseIdentifier: "CommunityCollectionViewCell")
+    }
+    
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func communityBackground() {
+        print("asdf")
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CommunityCollectionViewCell", for: indexPath)
+        
+        return cell
+    }
+        
+    override func collectionView(_ collectionView :UICollectionView , didSelectItemAt indexPath :IndexPath){
+        print(indexPath.row)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let size = CGSize(width: UIScreen.main.bounds.size.width, height: 80)
+        return size
     }
 }
