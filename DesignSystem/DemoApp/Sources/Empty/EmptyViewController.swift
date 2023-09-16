76c320e368491ba96d05f8fb4a26af88b2b3557c
junha <goodjunha@gmail.com>
2023-09-16 19:06:08 +0900

    💵:: StatusLayoutView 만듬

---
+++ DesignSystem/DemoApp/Sources/Empty/EmptyViewController.swift
@@ -0,0 +1,25 @@
+import Foundation
+import UIKit
+import SnapKit
+import RxCocoa
+import RxSwift
+import ResourceKit
+
+public class EmptyViewController: UIViewController {
+    private let disposeBag = DisposeBag()
+    
+    let containerView = StatusLayoutView(message: "asdf", image: ResourceKitAsset.memoXIcon.image)
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(containerView)
+        
+        containerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
