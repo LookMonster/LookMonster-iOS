8441ce9b6e69146730734aee040baa9cb6fb2400
junha <goodjunha@gmail.com>
2023-09-19 15:18:21 +0900

    🕯️::BannerViewController 추가

--- DesignSystem/DemoApp/Sources/Banner&&Indicator/BannerViewController.swift
+++ DesignSystem/DemoApp/Sources/Banner&&Indicator/BannerViewController.swift
@@ -5,31 +5,95 @@ import RxCocoa
 import RxSwift
 import ResourceKit
 
-public class BannerViewController: UIViewController {
-    private var monsterBannerView: MonsterBannerView!
+enum TestBannerCell {
+    case banner(model: MonsterBannerModel)
+}
 
+public class BannerViewController: UIViewController {
+    
+    private var viewModel: MonsterBannerModel?
+    private var cellList: [UITableViewCell] = []
+    private var cells: [TestBannerCell] = []
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .systemBackground
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.register(
+            MonsterBannerTableViewCell.self,
+            forCellReuseIdentifier: MonsterBannerTableViewCell.identifier
+        )
+        return tableView
+    }()
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewModel = MonsterBannerModel()
+        self.configureUI()
+        self.addCells()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+    }
+}
 
-        let viewModel = HomeViewModel()
-        monsterBannerView = MonsterBannerView(viewModel: viewModel)
-        monsterBannerView.backgroundColor = .white
-        monsterBannerView.imageList = [
-            ResourceKitAsset.kreamTestImage1.image,
-            ResourceKitAsset.kreamTestImage2.image,
-            ResourceKitAsset.kreamTestImage3.image,
-            ResourceKitAsset.kreamTestImage4.image
-        ]
-
-        view.addSubview(monsterBannerView)
-
-        monsterBannerView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
+extension BannerViewController {
+    private func configureUI() {
+        self.view.addSubview(self.tableView)
+        self.tableView.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
-            $0.height.equalTo(400)
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+    private func addCells() {
+        guard let viewModel = self.viewModel else { return }
+        self.cells.append(.banner(model: viewModel))
+    }
+}
+
+extension BannerViewController: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return self.view.frame.height / 2
+        } else if indexPath.row == 1 {
+            return self.view.frame.height / 3.55
+        } else {
+            return self.view.frame.height / 2
+        }
+    }
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("클릭")
+    }
+    public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        switch self.cells[indexPath.item] {
+        case .banner:
+            cell.separatorInset = UIEdgeInsets(top: 0, left: self.tableView.bounds.width, bottom: 0, right: 0)
+        }
+    }
+}
+
+extension BannerViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.cells.count
+    }
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let viewModel = self.viewModel else { return UITableViewCell() }
+        switch self.cells[indexPath.item] {
+        case .banner:
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: MonsterBannerTableViewCell.identifier
+            ) as? MonsterBannerTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.setUp(viewModel)
+            return cell
         }
     }
 }
