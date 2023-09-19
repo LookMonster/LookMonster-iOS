774bf7704c68590fc2377d770e8e1d49715b647c
junha <goodjunha@gmail.com>
2023-09-19 10:35:57 +0900

    💵:: banercell 완성

---
+++ DesignSystem/Sources/MonsterBannerView/Monster+BannerCell.swift
@@ -0,0 +1,36 @@
+import UIKit
+import Then
+import SnapKit
+
+class MonsterBannerCell: UICollectionViewCell {
+    
+    static var identifier: String = "MonsterBannerCell"
+    
+    private lazy var bannerImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
+
+extension MonsterBannerCell {
+    func configure(with bannerImage: UIImage) {
+         bannerImageView.image = bannerImage
+     }
+    
+    private func configureUI() {
+        addSubview(bannerImageView)
+        bannerImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
