b05b2c3d5778cb16596469f158b9818324d6f515
junha <goodjunha@gmail.com>
2023-09-24 22:06:45 +0900

    🕯️:: 테스트 이미지 추가

--- DesignSystem/Sources/MonsterProductCell/Monster+ProductCollectionViewCell.swift
+++ DesignSystem/Sources/MonsterProductCell/Monster+ProductCollectionViewCell.swift
@@ -13,7 +13,7 @@ public class MonsterProductCollectionViewCell: UICollectionViewCell {
     
     static public var identifier: String = "MonsterProductCollectionViewCell"
 
-    private lazy var goodsImageView: UIImageView = {
+    private lazy var productImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.layer.cornerRadius = 8.0
@@ -28,20 +28,20 @@ public class MonsterProductCollectionViewCell: UICollectionViewCell {
         button.tintColor = .black
         return button
     }()
-    private lazy var goodsNameLabel: UILabel = {
+    private lazy var productNameLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
         label.textAlignment = .left
         return label
     }()
-    private lazy var goodsDescriptionLabel: UILabel = {
+    private lazy var productDescriptionLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
         label.textAlignment = .left
         label.numberOfLines = 2
         return label
     }()
-    private lazy var goodsPriceLabel: UILabel = {
+    private lazy var priceLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
         label.textAlignment = .left
@@ -55,55 +55,55 @@ public class MonsterProductCollectionViewCell: UICollectionViewCell {
         label.text = ProductText.immediatelyText.rawValue
         return label
     }()
-    func setUp(_ goodsImage: UIImage, _ goodsName: String, _ goodsDescription: String, _ goodsPrice: String) {
+    func setUp(_ productImage: UIImage, _ productName: String, _ productDescription: String, _ productPrice: String) {
         self.configureUI()
-        self.goodsImageView.image = goodsImage
-        self.goodsNameLabel.text = goodsName
-        self.goodsDescriptionLabel.text = goodsDescription
-        self.goodsPriceLabel.text = goodsPrice
+        self.productImageView.image = productImage
+        self.productNameLabel.text = productName
+        self.productDescriptionLabel.text = productDescription
+        self.priceLabel.text = productPrice
         
-        self.goodsDescriptionLabel.sizeToFit()
+        self.productDescriptionLabel.sizeToFit()
     }
 }
 extension MonsterProductCollectionViewCell {
     private func configureUI() {
-        self.addSubview(self.goodsImageView)
-        self.goodsImageView.snp.makeConstraints {
+        self.addSubview(self.productImageView)
+        self.productImageView.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
-            $0.height.equalTo(self.goodsImageView.snp.width)
+            $0.height.equalTo(self.productImageView.snp.width)
         }
         self.addSubview(self.bookMarkButton)
         self.bookMarkButton.snp.makeConstraints {
-            $0.trailing.equalTo(self.goodsImageView.snp.trailing).inset(8)
-            $0.bottom.equalTo(self.goodsImageView.snp.bottom).inset(8)
+            $0.trailing.equalTo(self.productImageView.snp.trailing).inset(8)
+            $0.bottom.equalTo(self.productImageView.snp.bottom).inset(8)
             $0.width.equalTo(18)
             $0.height.equalTo(18)
         }
-        self.addSubview(self.goodsNameLabel)
-        self.goodsNameLabel.snp.makeConstraints {
-            $0.top.equalTo(self.goodsImageView.snp.bottom).offset(8)
-            $0.leading.equalTo(self.goodsImageView.snp.leading).inset(4)
-            $0.trailing.equalTo(self.goodsImageView.snp.trailing).inset(4)
+        self.addSubview(self.productNameLabel)
+        self.productNameLabel.snp.makeConstraints {
+            $0.top.equalTo(self.productImageView.snp.bottom).offset(8)
+            $0.leading.equalTo(self.productImageView.snp.leading).inset(4)
+            $0.trailing.equalTo(self.productImageView.snp.trailing).inset(4)
         }
-        self.addSubview(self.goodsDescriptionLabel)
-        self.goodsDescriptionLabel.snp.makeConstraints {
-            $0.top.equalTo(self.goodsNameLabel.snp.bottom)
-            $0.leading.equalTo(self.goodsNameLabel.snp.leading)
-            $0.trailing.equalTo(self.goodsNameLabel.snp.trailing)
+        self.addSubview(self.productDescriptionLabel)
+        self.productDescriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(self.productNameLabel.snp.bottom)
+            $0.leading.equalTo(self.productNameLabel.snp.leading)
+            $0.trailing.equalTo(self.productNameLabel.snp.trailing)
         }
-        self.addSubview(self.goodsPriceLabel)
-        self.goodsPriceLabel.snp.makeConstraints {
-            $0.top.equalTo(self.goodsDescriptionLabel.snp.bottom).offset(11)
-            $0.leading.equalTo(self.goodsNameLabel.snp.leading)
-            $0.trailing.equalTo(self.goodsNameLabel.snp.trailing)
+        self.addSubview(self.priceLabel)
+        self.priceLabel.snp.makeConstraints {
+            $0.top.equalTo(self.productDescriptionLabel.snp.bottom).offset(11)
+            $0.leading.equalTo(self.productNameLabel.snp.leading)
+            $0.trailing.equalTo(self.productNameLabel.snp.trailing)
         }
         self.addSubview(self.immediatelyBuyLabel)
         self.immediatelyBuyLabel.snp.makeConstraints {
-            $0.top.equalTo(self.goodsPriceLabel.snp.bottom)
-            $0.leading.equalTo(self.goodsNameLabel.snp.leading)
-            $0.trailing.equalTo(self.goodsNameLabel.snp.trailing)
+            $0.top.equalTo(self.priceLabel.snp.bottom)
+            $0.leading.equalTo(self.productNameLabel.snp.leading)
+            $0.trailing.equalTo(self.productNameLabel.snp.trailing)
         }
     }
 }
