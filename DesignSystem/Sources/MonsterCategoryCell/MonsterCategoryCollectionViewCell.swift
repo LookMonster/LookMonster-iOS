37e84caee28f92bdcf4c3728a8927f9513ac6846
junha <goodjunha@gmail.com>
2023-09-24 14:28:10 +0900

    🥊:: cell cornerRadius 변경

--- DesignSystem/Sources/MonsterCategoryCell/MonsterCategoryCollectionViewCell.swift
+++ DesignSystem/Sources/MonsterCategoryCell/MonsterCategoryCollectionViewCell.swift
@@ -1,14 +1,16 @@
 import UIKit
 import SnapKit
 import Then
+import ResourceKit
 
 final class MonsterCategoryCollectionViewCell: UICollectionViewCell {
     
     static var identifier: String = "MonsterCategoryCollectionViewCell"
 
     private lazy var recommendImageView = UIImageView().then {
-        $0.contentMode = .scaleAspectFit
-        $0.layer.cornerRadius = $0.frame.width / 2
+//        $0.contentMode = .scaleAspectFit
+        $0.backgroundColor = ResourceKitAsset.gray50.color
+//        $0.layer.cornerRadius = $0.frame.width / 2
         $0.layer.masksToBounds = true
         $0.clipsToBounds = true
     }
@@ -23,24 +25,25 @@ final class MonsterCategoryCollectionViewCell: UICollectionViewCell {
         self.configureUI()
         self.recommendImageView.image = image
         self.titleLabel.text = text
+        
+        recommendImageView.layer.cornerRadius = 27.0
     }
 }
 
 extension MonsterCategoryCollectionViewCell {
     private func configureUI() {
-        self.addSubview(recommendImageView)
-        
-        recommendImageView.snp.makeConstraints {
+        self.addSubview(self.recommendImageView)
+        self.recommendImageView.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.leading.equalToSuperview()
+            $0.height.equalTo(54.0)
             $0.trailing.equalToSuperview()
         }
-        self.addSubview(titleLabel)
-        
-        titleLabel.snp.makeConstraints {
-            $0.top.equalTo(recommendImageView.snp.bottom).offset(7)
-            $0.leading.equalTo(recommendImageView)
-            $0.trailing.equalTo(recommendImageView)
+        self.addSubview(self.titleLabel)
+        self.titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.recommendImageView.snp.bottom).offset(7)
+            $0.leading.equalTo(self.recommendImageView)
+            $0.trailing.equalTo(self.recommendImageView)
         }
     }
 }
