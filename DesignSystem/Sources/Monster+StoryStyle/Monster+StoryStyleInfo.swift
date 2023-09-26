d017e522d4dae8470b1d3602f17e7d13a0b20c25
junha <goodjunha@gmail.com>
2023-09-26 11:25:27 +0900

    🥊:: 디자인 시스템 접근 제어자 변경

--- DesignSystem/Sources/Monster+StoryStyle/Monster+StoryStyleInfo.swift
+++ DesignSystem/Sources/Monster+StoryStyle/Monster+StoryStyleInfo.swift
@@ -5,7 +5,7 @@ public struct MonsterStoryStyleInfo {
     public let userImage: UIImage
     public let userName: String
     
-    init(styleImage: UIImage, userImage: UIImage, userName: String) {
+    public init(styleImage: UIImage, userImage: UIImage, userName: String) {
         self.styleImage = styleImage
         self.userImage = userImage
         self.userName = userName
