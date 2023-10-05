6c1c367e378c617f21f30ea144e0b14a5b907afa
junha <goodjunha@gmail.com>
2023-10-05 13:06:10 +0900

    🕯️:: 테스트 처리 추가

--- Features/User/Main/Community/CommunityUserInterface/DemoApp/Sources/CommunityMockComponent.swift
+++ Features/User/Main/Community/CommunityUserInterface/DemoApp/Sources/CommunityMockComponent.swift
@@ -1,11 +1,3 @@
-//
-//  CommunityMockComponent.swift
-//  CommunityUserInterfaceDemoApp
-//
-//  Created by 박준하 on 2023/08/29.
-//  Copyright © 2023 lookMonster.io. All rights reserved.
-//
-
 import UIKit
 import RIBs
 import DesignSystem
