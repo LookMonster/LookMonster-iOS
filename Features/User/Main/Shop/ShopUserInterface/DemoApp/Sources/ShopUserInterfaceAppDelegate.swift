53fe9f295e00b239a07f31bc8f0e11ad35039f40
junha <goodjunha@gmail.com>
2023-10-10 21:24:18 +0900

    🥊:: appDelegate 수정

--- Features/User/Main/Shop/ShopUserInterface/DemoApp/Sources/ShopUserInterfaceAppDelegate.swift
+++ Features/User/Main/Shop/ShopUserInterface/DemoApp/Sources/ShopUserInterfaceAppDelegate.swift
@@ -1,13 +1,54 @@
 import UIKit
+import RIBs
+import DesignSystem
+import ShopUserInterface
+import ShopUserInterfaceImpl
+import ShopData
+import ShopDataImpl
+import ShopDomain
+import ShopDomainImpl
+import TokenManager
+import TokenManagerImpl
+import LKNetwork
+import LKNetworkImpl
+
+import RxSwift
+import RxCocoa
 
 @UIApplicationMain
 class ShopUserInterfaceAppDelegate: UIResponder, UIApplicationDelegate {
+//    var window: UIWindow?
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        let window = UIWindow(frame: UIScreen.main.bounds)
+//        window.rootViewController = ShopHomeViewController()
+//        window.makeKeyAndVisible()
+//        self.window = window
+//        return true
+//    }
+    
+    private final class MockAppShopListener: ShopHomeListener {
+        func communityBackground() {
+            fatalError()
+        }
+    }
+    
     var window: UIWindow?
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    var router: ViewableRouting?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = ShopUserInterfaceViewController()
+        let shopComponent = ShopMockComponent()
+        
+        router = ShopHomeBuilder(dependency: shopComponent)
+            .build(withListener: MockAppShopListener())
+        router?.interactable.activate()
+        
+        let navigationController = UINavigationController(rootViewController: self.router!.viewControllable.uiviewController)
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
+        
         return true
     }
 }
