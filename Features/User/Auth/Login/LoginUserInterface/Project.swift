9a643b1b25ce19c5a841398c21d65a5a9e36e4f4
junha <goodjunha@gmail.com>
2023-10-10 14:51:14 +0900

    🌧️:: userAuth 페이지 안쓰는 코드 삭제

--- Features/User/Auth/Login/LoginUserInterface/Project.swift
+++ Features/User/Auth/Login/LoginUserInterface/Project.swift
@@ -7,28 +7,19 @@ let project = Project.invertedDualTargetProjectWithDemoApp(
     platform: .iOS,
     iOSTargetVersion: "15.0.0",
     interfaceDependencies: [
-//        .Feature.Finance.Domain.Interface,
-        
         .Core.RIBsUtil,
         .TokenManager.Interface
     ],
     implementDependencies: [
-//        .Feature.Finance.Domain.Interface,
-//        .Feature.Finance.Data.Interface,
-        
         .Core.SuperUI,
         .Core.DefaultsStore,
         
-//        .ThirdParty.SPM.RxSwift,
-//        .ThirdParty.SPM.RxRelay,
-//        .ThirdParty.SPM.SnapKit,
-//        .ThirdParty.SPM.Swinject,
         .DesignSystem.Implement
     ],
     demoAppDependencies: [
         .Features.User.Auth.Login.Data.Implement,
         .Features.User.Auth.Login.Domain.Implement,
-//        .ThirdParty.SPM.Swinject,
+
         .LKNetwork.Implement,
         .TokenManager.Implement
     ],
