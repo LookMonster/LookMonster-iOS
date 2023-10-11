246e01e019288d195ded828933fdaff19588aeee
junha <goodjunha@gmail.com>
2023-10-11 13:54:12 +0900

    🥊:: 안쓰는 거 수정

--- Features/User/Main/Shop/ShopUserInterface/Interface/AppShop/All/AllBuildable.swift
+++ Features/User/Main/Shop/ShopUserInterface/Interface/AppShop/All/AllBuildable.swift
@@ -1,7 +1,7 @@
 import RIBs
-import SuperUI
-
+import DesignSystem
+ 
 public protocol AllBuildable: Buildable {
     // Interface 로 보내주세요
-    func build(withListener listener: AllListener, monsterCategory: MonsterCategory?) -> ViewableRouting
+    func build(withListener listener: AllListener) -> ViewableRouting
 }
